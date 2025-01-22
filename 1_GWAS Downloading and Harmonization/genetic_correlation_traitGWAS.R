4af2630d27c84333cbac1608e990069d618ad970
Lingxuan Zhu <152695684+LingxuanZ@users.noreply.github.com>
2025-01-22 13:18:32 -0500

    Genetic Correlation for immune cell traits and select 34 traits

---
+++ 1_GWAS Downloading and Harmonization/genetic_correlation_traitGWAS.R
@@ -0,0 +1,3 @@
+# This file is intended to quantify and evaluate the genetic correlation between different trait GWASs (especially in immune cell GWASs), 
+# as the number of immune cell GWASs is too large and the descriptions of the traits appear to be highly correlated.
+
